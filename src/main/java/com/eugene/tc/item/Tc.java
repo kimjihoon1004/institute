1485abeae2c768daf2a72384926a0ab4d932a473
김지훈 <97016991+kimjihoon1004@users.noreply.github.com>
2023-03-10 18:50:29 +0900

    Add files via upload

---
+++ src/main/java/com/eugene/tc/item/Tc.java
@@ -0,0 +1,51 @@
+package com.sample.mybatis.dto;
+
+public class Tc {
+	private String tc_mail;
+	private String tc_name;
+	private String tc_pw;
+	private String tc_date;
+	private String tc_gender;
+	private String tc_phone;
+	
+	public String getTc_mail() {
+		return tc_mail;
+	}
+	public void setTc_mail(String tc_mail) {
+		this.tc_mail = tc_mail;
+	}
+	
+	public String getTc_name() {
+		return tc_name;
+	}
+	public void setTc_name(String tc_name) {
+		this.tc_name = tc_name;
+	}
+	
+	public String getTc_pw() {
+		return tc_pw;
+	}
+	public void setTc_pw(String tc_pw) {
+		this.tc_pw = tc_pw;
+	}
+	
+	public String getTc_date() {
+		return tc_date;
+	}
+	public void setTc_date(String tc_date) {
+		this.tc_date = tc_date;
+	}
+	public String getTc_gender() {
+		return tc_gender;
+	}
+	public void setTc_gender(String tc_gender) {
+		this.tc_gender = tc_gender;
+	}
+	public String getTc_phone() {
+		return tc_phone;
+	}
+	public void setTc_phone(String tc_phone) {
+		this.tc_phone = tc_phone;
+	}
+	
+}
