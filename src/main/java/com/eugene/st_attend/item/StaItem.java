713345a7494812df612b4d6c065a84baee7ec7c8
김지훈 <97016991+kimjihoon1004@users.noreply.github.com>
2023-03-30 14:36:30 +0900

    Add files via upload

---
+++ src/main/java/com/eugene/st_attend/item/StaItem.java
@@ -0,0 +1,41 @@
+package com.eugene.st_attend.item;
+
+public class StaItem {
+	private String staId;
+	private String staIn;
+	private String staOut;
+	private String staAbsent;
+	private String staPhone;
+	
+	public String getStaId() {
+		return staId;
+	}
+	public void setStaId(String staId) {
+		this.staId = staId;
+	}
+	public String getStaIn() {
+		return staIn;
+	}
+	public void setStaIn(String staIn) {
+		this.staIn = staIn;
+	}
+	public String getStaOut() {
+		return staOut;
+	}
+	public void setStaOut(String staOut) {
+		this.staOut = staOut;
+	}
+	public String getStaAbsent() {
+		return staAbsent;
+	}
+	public void setStaAbsent(String staAbsent) {
+		this.staAbsent = staAbsent;
+	}
+	public String getStaPhone() {
+		return staPhone;
+	}
+	public void setStaPhone(String staPhone) {
+		this.staPhone = staPhone;
+	}
+	
+}
