1bab01383b2df2bdab118ae0513433a07e74b1fd
김지훈 <97016991+kimjihoon1004@users.noreply.github.com>
2023-04-01 17:01:06 +0900

    Update StaCtrl.java

--- src/main/java/com/eugene/st_attend/ctrl/StaCtrl.java
+++ src/main/java/com/eugene/st_attend/ctrl/StaCtrl.java
@@ -1,5 +1,35 @@
 package com.eugene.st_attend.ctrl;
 
 public class StaCtrl {
-
+	private String attend_in;
+	private String attend_out;
+	private String attend_absent;
+	private String stId;
+	
+	public String getAttend_in() {
+		return attend_in;
+	}
+	public void setAttend_in(String attend_in) {
+		this.attend_in = attend_in;
+	}
+	public String getAttend_out() {
+		return attend_out;
+	}
+	public void setAttend_out(String attend_out) {
+		this.attend_out = attend_out;
+	}
+	public String getAttend_absent() {
+		return attend_absent;
+	}
+	public void setAttend_absent(String attend_absent) {
+		this.attend_absent = attend_absent;
+	}
+	public String getStId() {
+		return stId;
+	}
+	public void setStId(String stId) {
+		this.stId = stId;
+	}
+	
+	
 }
