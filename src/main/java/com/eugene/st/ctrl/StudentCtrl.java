54f808d4df02575d9f425e00799dc0dd1663f064
amooge <125438713+amooge@users.noreply.github.com>
2023-04-01 13:55:33 +0900

    Add files via upload

---
+++ src/main/java/com/eugene/st/ctrl/StudentCtrl.java
@@ -0,0 +1,109 @@
+package com.eugene.st.ctrl;
+
+import com.eugene.common.db.SqlSessionCtrl;
+import com.eugene.st.item.StudentItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class StudentCtrl extends SqlSessionCtrl{
+
+	public List<StudentItem> retrieveStudentList() {
+		List<StudentItem> stBean = new ArrayList<StudentItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession()){
+			stBean = session.selectList("StudentInfor.retrieveStudentList");
+		}
+		
+		return stBean;
+	}
+	
+	public List<StudentItem> PartialRetrieveStudentList(String stName) {
+		List<StudentItem> stBean = new ArrayList<StudentItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession()){
+			stBean = session.selectList("StudentInfor.PartialRetrieveStudentList", stName);
+		}
+		
+		return stBean;
+	}
+	
+	public StudentItem retireveStudent(StudentItem stBean) {
+		StudentItem st = new StudentItem();
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			st = session.selectOne("retrieveStudent", stBean);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+		}
+		
+		return st;
+	}
+	
+	public int insertStudent(StudentItem stBean) {
+		int success = -1;
+		SqlSession session = null;
+		
+		try{
+			session = sqlSessionFactory.openSession();
+			success = session.insert("StudentInfor.insertStudent",stBean);
+			System.out.println("\n insert " + stBean.getStName() + " success " + success + "\n");
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public int deleteStudent(StudentItem stBean) {
+		int success = -1;
+		SqlSession session = null;
+		
+		try{
+			session = sqlSessionFactory.openSession();
+			success = session.delete("StudentInfor.deleteStudent",stBean);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	
+	public int updateStudent(StudentItem stBean) {
+		int success = -1;
+		SqlSession session = null;
+		
+		try{
+			session = sqlSessionFactory.openSession();
+			success = session.update("StudentInfor.updateStudent",stBean);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	
+}
