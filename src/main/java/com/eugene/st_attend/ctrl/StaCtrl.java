9d5225e6f031a58818d37a396b06e9a2481263ad
김지훈 <97016991+kimjihoon1004@users.noreply.github.com>
2023-04-10 18:32:15 +0900

    Update StaCtrl.java

--- src/main/java/com/eugene/st_attend/ctrl/StaCtrl.java
+++ src/main/java/com/eugene/st_attend/ctrl/StaCtrl.java
@@ -1,35 +1,229 @@
 package com.eugene.st_attend.ctrl;
 
-public class StaCtrl {
-	private String attend_in;
-	private String attend_out;
-	private String attend_absent;
-	private String stId;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.eugene.common.db.SqlSessionCtrl;
+import com.eugene.st_attend.item.StaItem;
+
+public class StaCtrl extends SqlSessionCtrl{
 	
-	public String getAttend_in() {
-		return attend_in;
-	}
-	public void setAttend_in(String attend_in) {
-		this.attend_in = attend_in;
-	}
-	public String getAttend_out() {
-		return attend_out;
-	}
-	public void setAttend_out(String attend_out) {
-		this.attend_out = attend_out;
-	}
-	public String getAttend_absent() {
-		return attend_absent;
-	}
-	public void setAttend_absent(String attend_absent) {
-		this.attend_absent = attend_absent;
-	}
-	public String getStId() {
-		return stId;
-	}
-	public void setStId(String stId) {
-		this.stId = stId;
+	public int studentAttend(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		try {
+			session = sqlSessionFactory.openSession();
+			success = session.insert("StaSql.studentAttend", staItem);	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
 	}
 	
+	public int studentAasent(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		try {
+			session = sqlSessionFactory.openSession();
+			success = session.insert("StaSql.studentAbsent", staItem);	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public List<StaItem> checkAttend(String date)	{
+		List<StaItem> staList = new ArrayList<StaItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			staList = session.selectList("StaSql.checkAttend", date);
+		}
+		
+		return staList;
+	}
+	
+	public List<StaItem> retrieveDate(StaItem staItem)	{
+		List<StaItem> staList = new ArrayList<StaItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			staList = session.selectList("StaSql.retrieveDate", staItem);
+		}
+		
+		return staList;
+	}
+	
+	public int studentGoHome(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			success = session.update("StaSql.studentGoHome", staItem);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public String selectAttendId(StaItem staItem)	{
+		String id = "";
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			id = session.selectOne("StaSql.selectAttendId", staItem);
+		}
+		
+		return id;
+	}
+	
+	public int updateIn(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			success = session.update("StaSql.updateIn", staItem);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+
+	public int supplement(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			success = session.update("StaSql.supplement", staItem);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public int updateAttend(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		
+		try {
+			session = sqlSessionFactory.openSession();
+			success = session.update("StaSql.updateAttend", staItem);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public List<StaItem> retrieveAttend(String date)	{
+		List<StaItem> staList = new ArrayList<StaItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			staList = session.selectList("StaSql.retrieveAttend", date);
+		}
+		
+		return staList;
+	}
+	
+	public List<StaItem> retrieve(StaItem staItem)	{
+		List<StaItem> staList = new ArrayList<StaItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			staList = session.selectList("StaSql.retrieve", staItem);
+		}
+		
+		return staList;
+	}
+	
+	public List<StaItem> retrieveAbsent(String date)	{
+		List<StaItem> staList = new ArrayList<StaItem>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			staList = session.selectList("StaSql.retrieveAbsent", date);
+		}
+		
+		return staList;
+	}
+	
+	public List<String> findAbsent(String id)	{
+		List<String> temp = new ArrayList<String>();
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			temp = session.selectList("StaSql.findAbsent", id);
+		}
+		
+		return temp;
+	}
+	
+	public int deleteAbsent(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		
+		try{
+			session = sqlSessionFactory.openSession();
+			success = session.delete("StaSql.deleteAbsent", staItem);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public int deleteAttend(StaItem staItem)	{
+		int success = -1;
+		SqlSession session = null;
+		
+		try{
+			session = sqlSessionFactory.openSession();
+			success = session.delete("StaSql.deleteAttend", staItem);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			session.commit();
+			session.close();
+		}
+		return success;
+	}
+	
+	public int countAbsent(int id)	{
+		int temp = 0;
+		
+		try(SqlSession session = sqlSessionFactory.openSession())	{
+			temp = session.selectOne("StaSql.countAbsent", id);
+		}
+		
+		return temp;
+	}
 	
 }
